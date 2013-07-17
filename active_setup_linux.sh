b55bb32f06d0d83d3ecb87a03b055a4374a10a66
creyes <cer26@duke.edu>
2013-07-17 13:34:32 -0400

    Changed comments and installation path

--- active_setup_linux.sh
+++ active_setup_linux.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#Ubuntu
+#Script to set up activerecord tests on Ubuntu
+
 #installing dependencies
 sudo apt-get install libtool libxml2 openssl sqlite
 sudo apt-get install rbenv
@@ -19,7 +20,7 @@ gem install bundler
 echo "export NUODB_ROOT=/opt/nuodb/bin
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" #  Load RVM function" >> ~/.bash_profile
 
-git clone https://github.com/nuodb/ruby-nuodb.git ~/Documents/ruby-nuodb-latest
+git clone https://github.com/nuodb/ruby-nuodb.git /tmp/ruby-nuodb-latest
 cd ruby-nuodb-latest
 bundle
 rbenv rehash
@@ -30,11 +31,11 @@ rake clean build
 cd pkg
 gem install nuodb-1.0.2.gem
 
-cd ~/Documents
+cd /tmp
 
 #Getting the driver
-git clone https://github.com/nuodb/ruby-activerecord-nuodb-adapter.git ~/Documents/ruby-activerecord-nuodb-adapter-latest
-cd ~/Documents/ruby-activerecord-nuodb-adapter-latest
+git clone https://github.com/nuodb/ruby-activerecord-nuodb-adapter.git /tmp/ruby-activerecord-nuodb-adapter-latest
+cd /tmp/ruby-activerecord-nuodb-adapter-latest
 bundle
 rbenv rehash
 rake clean build
@@ -42,9 +43,9 @@ cd pkg
 gem install activerecord-nuodb-adapter-1.0.3.gem #Make robust
 
 #Getting rails
-cd ~/Documents
-git clone https://github.com/rails/rails.git ~/Documents/rails-latest
-cd ~/Documents/rails-latest
+cd /tmp
+git clone https://github.com/rails/rails.git /tmp/rails-latest
+cd /tmp/rails-latest
 git checkout v3.2.8
 bundle
 rbenv rehash
@@ -52,7 +53,7 @@ cd activerecord
 
 echo "if ENV['NUODB_AR']
     gem 'activerecord-nuodb-adapter'
-end" >> ~/Documents/rails-latest/Gemfile
+end" >> /tmp/rails-latest/Gemfile
 
 echo "
   nuodb:
@@ -68,19 +69,21 @@ echo "
       username: cloud
       password: user
       schema: test
-" >> ~/Documents/rails-latest/activerecord/test/config.example.yml
+" >> /tmp/rails-latest/activerecord/test/config.example.yml
 
 sed -i 's/%w( mysql mysql2 postgresql/%w( nuodb mysql mysql2 postgresql/g' Rakefile
 
-echo "gem 'activerecord-nuodb-adapter'" >> ~/Documents/rails-latest/Gemfile
+echo "gem 'activerecord-nuodb-adapter'" >> /tmp/rails-latest/Gemfile
 bundle install
 
-#Starting NuoDB
+# Helpful information, make sure that NuoDB is running
+
+# Starting NuoDB
 #java -jar /opt/nuodb/jar/nuoagent.jar --broker &
 #/opt/nuodb/bin/nuodb --chorus test --password bar --dba-user cloud --dba-password user --verbose debug --archive /var/tmp/nuodb --initialize --force &
 #/opt/nuodb/bin/nuodb --chorus test --password bar --dba-user cloud --dba-password user &
 
-#Running Tests
-#cd ~/Documents/rails-latest/activerecord
+# Running Tests
+#cd /tmp/rails-latest/activerecord
 #ARCONN=nuodb ruby -Itest test/cases/base_test.rb
 
