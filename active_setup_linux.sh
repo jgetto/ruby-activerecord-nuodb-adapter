0fbb8407c7e0c23be1e4a281b23063d5257ff696
creyes <cer26@duke.edu>
2013-07-26 12:22:15 -0400

    location changes

--- active_setup_linux.sh
+++ active_setup_linux.sh
@@ -2,29 +2,29 @@
 #Script to set up activerecord tests on Ubuntu
 
 #installing dependencies
-sudo apt-get update
-sudo apt-get install libtool libxml2 openssl sqlite
-sudo apt-get install rbenv
-sudo apt-get install ruby-build
+# sudo apt-get update
+# sudo apt-get install libtool libxml2 openssl sqlite
+# sudo apt-get install rbenv
+# sudo apt-get install ruby-build
 
 echo "eval \"\$(rbenv init -)\"" >> ~/.bash_profile
 
-rbenv install 1.9.3-p392
-rbenv global 1.9.3-p392
-rbenv rehash
+# rbenv install 1.9.3-p392
+# rbenv global 1.9.3-p392
+# rbenv rehash
 eval "$(rbenv init -)"
 
-sudo gem list | cut -d" " -f1 | xargs gem uninstall -aIx
-gem install bundler
+# sudo gem list | cut -d" " -f1 | xargs gem uninstall -aIx
+# gem install bundler
 
 # Writes to .bash_profile
 echo "export NUODB_ROOT=/opt/nuodb/bin
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" #  Load RVM function" >> ~/.bash_profile
 
-git clone https://github.com/nuodb/ruby-nuodb.git /tmp/ruby-nuodb-latest
-cd ruby-nuodb-latest
-bundle
-rbenv rehash
+# git clone https://github.com/nuodb/ruby-nuodb.git /tmp/ruby-nuodb-latest
+# cd ruby-nuodb-latest
+# bundle
+# rbenv rehash
 
 cd /tmp/ruby-nuodb-latest
 chmod 777 nuodb.gemspec
@@ -36,22 +36,22 @@ sudo gem install nuodb-1.0.2.gem
 cd /tmp
 
 #Getting the driver
-git clone https://github.com/nuodb/ruby-activerecord-nuodb-adapter.git /tmp/ruby-activerecord-nuodb-adapter-latest
-cd /tmp/ruby-activerecord-nuodb-adapter-latest
-bundle
-rbenv rehash
-rake clean build
-cd pkg
-gem install activerecord-nuodb-adapter-1.0.3.gem #Make robust
+# git clone https://github.com/nuodb/ruby-activerecord-nuodb-adapter.git /tmp/ruby-activerecord-nuodb-adapter-latest
+# cd /tmp/ruby-activerecord-nuodb-adapter-latest
+# bundle
+# rbenv rehash
+# rake clean build
+# cd pkg
+# gem install activerecord-nuodb-adapter-1.0.3.gem #Make robust
 
 #Getting rails
-cd /tmp
-git clone https://github.com/rails/rails.git /tmp/rails-latest
-cd /tmp/rails-latest
-git checkout v3.2.8
-bundle
-rbenv rehash
-cd activerecord
+# cd /tmp
+# git clone https://github.com/rails/rails.git /tmp/rails-latest
+# cd /tmp/rails-latest
+# git checkout v3.2.8
+# bundle
+# rbenv rehash
+# cd activerecord
 
 echo "if ENV['NUODB_AR']
     gem 'activerecord-nuodb-adapter'
